ff47c9091e51fb67feda40888ef28258c6279e99
mousetrap0 <james0619@naver.com>
2022-09-06 23:21:47 +0900

    first commit

---
+++ src/Components/app/Header.js
@@ -0,0 +1,12 @@
+import  React from "react";
+function Header() {
+	return (
+		<header className="py-4">
+			<div className="container text-center">
+				<img alt="" src="/images/airportlogo.png" width="1100" height="150" />
+			</div>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/Components/app/Home.js
@@ -0,0 +1,10 @@
+import  React from "react";
+function Home() {
+	return (
+		<div>
+			<h2>항행통신부 종합통신소 장애 관리</h2>
+		</div>
+	);
+}
+
+export default Home;
