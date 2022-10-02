064747ce63a6bfade830392dd88c31253a3f411f
bo <lazymonkey789@gmail.com>
2022-10-02 23:28:58 +0900

    chart 커스텀중

--- src/Components/chart/Chart.js
+++ src/Components/chart/Chart.js
@@ -12,24 +12,11 @@ import axios from "axios";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
 
-const apiData = [
-    { name: "test1", count: 3, time: 4 },
-    { name: "test1", count: 3, time: 4 },
-    { name: "test1", count: 3, time: 4 },
-
-    { name: "test2", count: 1, time: 4 },
-    { name: "test2", count: 1, time: 4 },
-
-    { name: "test3", count: 2, time: 11 },
-    { name: "test4", count: 7, time: 3 },
-    { name: "test5", count: 5, time: 3 },
-];
-
 const Chart = () => {
     const { auth, setAuth } = useContext(AuthContext);
 
     //차트 기간설정
-    const [termVal, setTermVal] = useState();
+    const [termVal, setTermVal] = useState("year");
 
     const [chartVal, setChartVal] = useState();
 
@@ -41,7 +28,15 @@ const Chart = () => {
             })
             .then((resp) => {
                 console.log(resp, "success");
-                setChartVal(resp.data.chartList);
+                const temp = resp.data.chartList.map((d) => {
+                    return {
+                        회선이름: d.lineId,
+                        "장애지속시간(분)": d.failTimeTotal,
+                        "장애횟수(건)": d.breakCount,
+                    };
+                });
+                setChartVal(temp);
+                console.log(temp);
             })
             .catch((err) => {
                 console.log(err, "fail");
@@ -49,8 +44,8 @@ const Chart = () => {
     };
 
     useEffect(() => {
-        initdata("year");
-    }, []);
+        initdata(termVal);
+    }, [termVal]);
 
     /* 차트 기간 선택 */
     const onSelClick = (e) => {
@@ -59,49 +54,82 @@ const Chart = () => {
 
     return (
         <div>
-            <div>
-                <div className="mb-3 d-flex justify-content-end">
-                    <button
-                        className="btn btn-outline-secondary"
-                        id="year"
-                        onClick={onSelClick}
-                    >
-                        연간
-                    </button>
-                    <button
-                        className="btn btn-outline-secondary"
-                        id="quarter"
-                        onClick={onSelClick}
-                    >
-                        분기
-                    </button>
-                    <button
-                        className="btn btn-outline-secondary"
-                        id="month"
-                        onClick={onSelClick}
-                    >
-                        월간
-                    </button>
-                </div>
-                <div className="mb-3 d-flex justify-content-center">
+            <div className="mb-3 d-flex justify-content-center">
+                <div>
+                    <div className="mb-3 mr-5 d-flex justify-content-end">
+                        <button
+                            className="btn btn-outline-secondary"
+                            id="year"
+                            onClick={onSelClick}
+                        >
+                            연간
+                        </button>
+                        <button
+                            className="btn btn-outline-secondary"
+                            id="quarter"
+                            onClick={onSelClick}
+                        >
+                            분기
+                        </button>
+                        <button
+                            className="btn btn-outline-secondary"
+                            id="month"
+                            onClick={onSelClick}
+                        >
+                            월간
+                        </button>
+                    </div>
                     <BarChart
                         width={1000}
                         height={350}
                         data={chartVal}
                         margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
+                            top: 50,
+                            right: 50,
+                            left: 50,
                             bottom: 5,
                         }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="lineId" />
-                        <YAxis />
+                        <XAxis
+                            dataKey="회선이름"
+                            padding={{ left: 20, right: 20 }}
+                        />
+                        <YAxis
+                            yAxisId="left"
+                            label={{
+                                value: "분",
+                                offset: 30,
+                                angle: 0,
+                                position: "top",
+                            }}
+                            type="number"
+                            domain={[0, (dataMax) => dataMax]}
+                        />
+                        <YAxis
+                            yAxisId="right"
+                            orientation="right"
+                            label={{
+                                value: "건",
+                                offset: 30,
+                                angle: 0,
+                                position: "top",
+                            }}
+                        />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="failTimeTotal" fill="#BC8F8F	" />
-                        <Bar dataKey="breakCount" fill="#B8860B	" />
+                        <Bar
+                            yAxisId="left"
+                            dataKey="장애지속시간(분)"
+                            barSize={termVal === "year" ? 50 : 100}
+                            fill="#BC8F8F	"
+                        />
+                        <Bar
+                            yAxisId="right"
+                            dataKey="장애횟수(건)"
+                            barSize={termVal === "year" ? 50 : 100}
+                            fill="#B8860B	"
+                        />
                     </BarChart>
                 </div>
             </div>
